f788e936d0e132cc3c8e6e1fb2560135d44e8429
Willem Mulder <willemmaster@hotmail.com>
2015-10-06 22:48:27 +0200

    Colorize conversation list

--- application/src/main/java/indrora/atomic/activity/ConversationActivity.java
+++ application/src/main/java/indrora/atomic/activity/ConversationActivity.java
@@ -840,8 +840,11 @@ public class ConversationActivity extends Activity implements
             .getCount()];
         for( int i = 0; i < pagerAdapter.getCount(); i++ ) {
           Conversation c = pagerAdapter.getItem(i);
-          CharSequence title = (c.getName().equals("") ? server.getTitle() : c
+          SpannableString chan = new SpannableString(c.getName().equals("") ? server.getTitle() : c
               .getName());
+          chan.setSpan(new ForegroundColorSpan(pagerAdapter.getColorAt(i)), 0, chan.length(),
+              SpannableString.SPAN_INCLUSIVE_INCLUSIVE);
+          CharSequence title = chan;
           if( c.getNewMentions() > 0 ) {
             SpannableString unread = new SpannableString("("
                 + c.getNewMentions() + ")");
@@ -849,7 +852,7 @@ public class ConversationActivity extends Activity implements
                 new ForegroundColorSpan(getResources().getColor(
                     android.R.color.secondary_text_dark_nodisable)), 0,
                 unread.length(), SpannableString.SPAN_INCLUSIVE_INCLUSIVE);
-            title = TextUtils.concat(title, " ", unread);
+            title = TextUtils.concat(chan, " ", unread);
           }
           conversationsArr[i] = title;
         }
